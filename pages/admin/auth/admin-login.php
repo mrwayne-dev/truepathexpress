559cfcfa4a2d78c798669b471436c1d67afdbe71
Michael <aleruchi0987@gmail.com>
2026-02-15 21:32:41 +0100

    main

---
+++ pages/admin/auth/admin-login.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<!--<![endif]-->
+
+
+<!-- Mirrored from themesflat.co/html/critso-admin/sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Oct 2025 21:59:48 GMT -->
+<head>
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>Critso - Crypto Dashboard Template</title>
+
+    <meta name="author" content="themesflat.com">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animation.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+
+    <!-- Font -->
+    <link rel="stylesheet" href="font/fonts.css">
+
+    <!-- Icon -->
+    <link rel="stylesheet" href="icon/style.css">
+
+    <!-- Favicon and Touch Icons  -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="images/favicon.png">
+
+</head>
+
+<body>
+
+    <!-- #wrapper -->
+    <div id="wrapper">
+        <!-- #page -->
+        <div id="page" class="">
+            <div class="sign-in-wrap">
+                <div class="sign-in-box">
+                    <div class="left">
+                        <div class="content">
+                            <h3 class="heading">
+                                Hello Kampbail
+                            </h3>
+                            <div class="sub f12-regular text-GrayDark">Welcome again to our website</div>
+                            <div class="sign-in-inner">
+                                <h4>Sign In Here</h4>
+                                <form class="form-login flex flex-column gap24" accept-charset="utf-8" action="https://themesflat.co/html/critso-admin/index.html">
+                                    <fieldset class="email">
+                                        <div class="f14-regular mb-6">Email here</div>
+                                        <input class="flex-grow" type="email" placeholder="Enter your email address" name="email" tabindex="0" value="keplamdusa@gmail.com" aria-required="true" required="">
+                                    </fieldset>
+                                    <fieldset class="password">
+                                        <div class="f14-regular mb-6">Password here</div>
+                                        <input class="password-input" type="password" placeholder="Enter your password" name="password" tabindex="0" value="12345" aria-required="true" required="">
+                                        <span class="show-pass">
+                                            <i class="icon-view view"></i>
+                                            <i class="icon-hide hide"></i>
+                                        </span>
+                                    </fieldset>
+                                    <div class="tf-cart-checkbox">
+                                        <div class="tf-checkbox-wrapp">
+                                            <input class="checkbox-item" type="checkbox" name="transaction_checkbox" checked="">
+                                            <div>
+                                                <i class="icon-check"></i>
+                                            </div>
+                                        </div>
+                                        <div class="f14-regular">
+                                            By proceeding, you agree to the 
+                                            <a href="#" class="f14-bold">Terms and Conditions</a>
+                                        </div>
+                                    </div>
+                                    <button class="tf-button style-1 label-01 w-100">Sign In</button>
+                                </form>
+                                <div class="f14-regular">
+                                    Don’t have an account? 
+                                    <a href="sign-up.html" class="f14-bold">Sign Up</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <img src="images/item/bg-sign-in.jpg" alt="">
+                        <div class="text">
+                            <img src="images/item/sign-in.png" alt="">
+                            <h3 class="mb-24 text-White">Buy & sell Each Digital <br> Cryptocurrency</h3>
+                            <div class="f14-regular text-White">
+                                Easily Buy bitcoin and other cryptocurrencies using <br>
+                                wide range of payment option. Discover exclusive <br>
+                                digital collection of us.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page -->
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Javascript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+
+<!-- Mirrored from themesflat.co/html/critso-admin/sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Oct 2025 21:59:49 GMT -->
+</html>
